a3e1e6bccc0e183ae8db5d68b775e64510fe41ab
Juliette Simonot <97804189+juliettesimonot@users.noreply.github.com>
2022-10-25 12:00:17 +0200

    api getallFilms

--- rdv_serveur/src/main/kotlin/com/example/rdv_serveur/repository/FilmRepository.kt
+++ rdv_serveur/src/main/kotlin/com/example/rdv_serveur/repository/FilmRepository.kt
@@ -1,4 +1,9 @@
 package com.example.rdv_serveur.repository
 
-class FilmRepository {
+import com.example.rdv_serveur.model.FilmBean
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface FilmRepository: JpaRepository<FilmBean, Int> {
 }
